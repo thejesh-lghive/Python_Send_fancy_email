56529b6a7768af72a11c1e7f44d459423728dea5
Thejesh <50484218+thejesh-lghive@users.noreply.github.com>
2019-11-10 19:13:27 +0530

    Boadcast mail
    We read "contacts_files.csv file" to get reciever data.

--- emailsend.py
+++ emailsend.py
@@ -2,15 +2,15 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import statusicon
+import csv
 
 sender_email = "mrstoni78@gmail.com"
-receiver_email = "theju.mrss123@gmail.com"
 password = input('Enter Password')
 
 message = MIMEMultipart("alternative")
 message["Subject"] = "multipart test"
 message["From"] = sender_email
-message["To"] = receiver_email
+
 
 totalworkorder = "6"
 escalatedsites = "10"
@@ -33,93 +33,56 @@ How are you?
 Real Python has many great tutorials:
 www.realpython.com"""
 html = """<!DOCTYPE html>
-<html>
-<head>
-<title>Bootstrap Icons</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-</head>
-<body class="container">
-<div style="width:97%;display: inline-flex;border-style:  groove;box-shadow: inset 0 0 3px 1px black;background-color: white;"> 
-    <div style="width:70%;background-image: linear-gradient(smoke, white);/* border-style: double; */display:block;padding:1%;">
-    
-    <center><h1>TODAYS WORK STATUS</h1></center>
+<div style="width:97%;display:flex;border-style:  groove;box-shadow: inset 0 0 3px 1px black;background-color: white;">
+  <div class="" style="width:70%;background-image: linear-gradient(smoke, white);display:block;padding:1%;">
+    <center>
+      <h1>TODAYS WORK STATUS</h1>
+    </center>
     <br>
-   
-    <div style=" display: inline-block;
-      width: 96%;
-      border-style: double;  
-      background-image: linear-gradient(whit, white); ">
-      <center>
+
+   <div style=" display: inline-block;width: 100%;border-style: double; background-image: linear-gradient(whit, white); ">
         <div style="margin: 3px;">
-        <div style="
-        display: inline-block;
-        width: 24%;
-        padding-top:10px;
-        background-image: linear-gradient(gray, white);
-        "><center>Total Work<br/> Order<br><h1>"""+totalworkorder+"""</h1></center></div>
-        <div style="
-        display: inline-block;
-        width: 24%;
-        padding-top:10px;
-        background-image: linear-gradient(gray, white);
-        "><center>Escalated <br/>Sites<br><h1>"""+escalatedsites+"""</h1></center></div>
-        <div style="
-        display: inline-block;
-        width: 24%;
-        padding-top:10px;
-        background-image: linear-gradient(gray, white);
-        "><center>Flagged <br/>Sites<br><h1>"""+flaggedsites+"""</h1></center></div>
-        <div style="
-        display: inline-block;
-        width: 24%;
-        padding-top:10px;
-        background-image: linear-gradient(gray, white);
-        "><center>Calls in<br/> Que<br><h1>"""+callsinque+"""</h1></center></div>
+          <div style="display: inline-block;width: 40%;padding-top:10px;background-image: linear-gradient(gray, white);">
+            <center>Total Work<br /> Order<br><h1>10</h1></center>
+          </div>
+          <div style="display: inline-block;width: 40%;padding-top:10px;background-image: linear-gradient(gray, white);">
+            <center>Escalated <br />Sites<br><h1>10</h1></center>
+          </div>
+          <div style="display: inline-block;width: 40%;padding-top:10px;background-image: linear-gradient(gray, white);">
+            <center>Flagged <br />Sites<br><h1>10</h1></center>
+          </div>
+          <div style="display: inline-block;width: 40%;padding-top:10px;background-image: linear-gradient(gray, white);">
+            <center>Calls in<br /> Que<br><h1>100</h1></center>
+          </div>
+        </div>
+      <br/>
+    
+        <div style="margin: 3px;">
+          <div style="display: inline-block;width: 40%;padding-top:10px;background-image: linear-gradient(gray, white);">
+            <center>Calls<br /> Active<br><h1>100</h1></center>
+          </div>
+          <div style="display: inline-block;width: 40%;padding-top:10px;background-image: linear-gradient(gray, white);">
+            <center>Calls<br /> Completed<br><h1>100</h1></center>
+          </div>
+          <div style="display: inline-block;width: 40%;padding-top:10px;background-image: linear-gradient(gray, white);">
+            <center>Notifications <br />Sent<br><h1>100</h1></center>
+          </div>
+          <div style="display: inline-block;width: 40%;padding-top:10px;background-image: linear-gradient(gray, white);">
+            <center>Notifications<br /> Failed<br><h1>100</h1></center>
+          </div>
+        </div>
+      
+    </div>
+  </div>
+  <div 	class="" style="border-style: double;margin: 14px;width:30%;display:block;padding:1%;box-shadow: 0 0 4px black;background-color: white;">
+    <center>
+      <p style="margin-top:50%;">
+        <img src="""+Status__ICON+""" width="150px" height="150px" alt="Good">
+      </p>
+    </center>
+    <span>Today's progress determined by yesterday's choices </span>
+  </div>
 </div>
-</center>
-<br/>
-<center>
-    <div style="margin: 3px;">
-    <div style="
-    display: inline-block;
-    width: 24%;
-    padding-top:10px;
-    background-image: linear-gradient(gray, white);
-    "><center>Calls<br/> Active<br><h1>"""+callsactive+"""</h1></center></div>
-    <div style="
-    display: inline-block;
-    width: 24%;
-    padding-top:10px;
-    background-image: linear-gradient(gray, white);
-    "><center>Calls<br/> Completed<br><h1>"""+callscomplted+"""</h1></center></div>
-    <div style="
-    display: inline-block;
-    width: 24%;
-    padding-top:10px;
-    background-image: linear-gradient(gray, white);
-    "><center>Notifications <br/>Sent<br><h1>"""+notificationssent+"""</h1></center></div>
-    <div style="
-    display: inline-block;
-    width: 24%;
-    padding-top:10px;
-    background-image: linear-gradient(gray, white);
-    "><center>Notifications<br/> Failed<br><h1>"""+notificationsfailed+"""</h1></center></div>
-    </div>
-</center>
-      </div>
-    </div>
-     <div style="border-style: double;margin: 14px;width:30%;display:block;padding:1%;/* border: 1px solid; */box-shadow: 0 0 4px black;background-color: white;">
-     <center>
-            <p style="margin-top:50%;">
-            <img src="""+Status__ICON+""" width="150px" height="150px" alt="Good">
-            </p>
-            </center>
-     <span>Today's progress determined by yesterday's choices </span>
-    </div>
-    </div>
-    </body>
-    </html>
 """
 
 # Turn these into plain/html MIMEText objects
@@ -133,8 +96,13 @@ message.attach(part2)
 
 # Create secure connection with server and send email
 context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(
-        sender_email, receiver_email, message.as_string()
-    )
+with open("contacts_file.csv") as file:
+    reader = csv.reader(file)
+    next(reader)  # Skip header row
+    for name, email in reader:
+        print(f"Sending email to {name}")
+        receiver_email = email
+        message["To"] = receiver_email
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
